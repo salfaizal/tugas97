a0619e5f728b200bdbae9667c1cc6b04de227004
salfaizal <32302330+salfaizal@users.noreply.github.com>
2018-01-23 01:27:11 +0800

    merapikan input data

--- Untitled5.cpp
+++ Untitled5.cpp
@@ -21,6 +21,23 @@ int main(){
 	scorpio.typecustom	= "chopper ";
 	scorpio.tenagamesin 	= 300 ;
 //isi data glpro
-	glpro.typecustom 		= "kuning hitam ";
+	glpro.typecustom 		= "caferacer ";
 	glpro.tenagamesin		= 150 ;
-
+	
+//cout tiger	
+	cout << (tiger.typecustom);
+	cout << (tiger.tenagamesin)<<" ";
+	tiger.jenismotor("honda tiger ");
+//cout scorpio
+	cout << (scorpio.typecustom);
+	cout << (scorpio.tenagamesin)<<" ";
+	scorpio.jenismotor("yamaha scorpio ");
+//cout glpro
+	cout << (glpro.typecustom);
+	cout << (glpro.tenagamesin)<<" ";
+	glpro.jenismotor("honda gl-pro ");
+}
+//output void
+void motorcustom ::jenismotor(string dibuat){
+	cout<<"jenis motornya "<<dibuat<<endl;
+}
